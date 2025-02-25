3d4fdc8c5456c08e187e5357d8435c47fe98cedb
adeshile <shileosun@gmail.com>
2025-02-25 15:54:52 +0000

    Updated github file

--- backend/app.py
+++ backend/app.py
@@ -3,8 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/api')
+
+
 def api():
     return "Hello from the Backend!"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5002)
