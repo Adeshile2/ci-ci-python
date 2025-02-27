29db9a50b0f1b7c8da852cae32bdd3e460d51348
Adeshile Osunkoya <67486486+Adeshile2@users.noreply.github.com>
2025-02-27 16:02:11 +0000

    Update ecr.tf

--- Terraform/ecr.tf
+++ Terraform/ecr.tf
@@ -35,7 +35,7 @@ resource "aws_ecr_repository" "my_ecr_repo" {
 }
 #Output the ecr repo
 output "aws_ecr_repo" {
-  value = aws_ecr_repository.my_ecr_repo
+  value = aws_ecr_repository.my_ecr_repo.arn
 }
 
 
