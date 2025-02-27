076668de3d1294bd68c9586d98e498d141554c40
adeshile <shileosun@gmail.com>
2025-02-27 10:36:40 +0000

    Update test

---
+++ Terraform/ecr.tf
@@ -0,0 +1,40 @@
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create a secret in AWS Secrets Manager
+resource "aws_secretsmanager_secret" "env_secrets" {
+  name = "my-env-secrets" # Name of the secret
+  description = "Secrets for environment variables"
+
+}
+
+# Store key-value pairs in the secret
+resource "aws_secretsmanager_secret_version" "env_secrets_version" {
+  secret_id = aws_secretsmanager_secret.env_secrets.id
+  secret_string = jsonencode({
+    AWS_ACCESS_KEY_ID     = var.aws_access_key
+    AWS_SECRET_ACCESS_KEY = var.aws_secret_key
+    
+  })
+}
+
+# Output the secret ARN
+output "secret_arn" {
+  value = aws_secretsmanager_secret.env_secrets.arn
+}
+
+resource "aws_ecr_repository" "my_ecr_repo" {
+  name                 = "my-ecr-devops" # Replace with your desired repository name
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+
+
+
+
---
+++ Terraform/variables.tf
@@ -0,0 +1,18 @@
+# variables.tf
+variable "aws_access_key" {
+  description = "AWS Access Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "us-east-1"
+}
