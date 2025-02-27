682958b8f108d834c065d6956a0e22e54bf042f2
Adeshile Osunkoya <67486486+Adeshile2@users.noreply.github.com>
2025-02-27 14:20:13 +0000

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -10,3 +10,9 @@ variable "aws_secret_key" {
   type        = string
   sensitive   = true
 }
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "us-east-1"
+}
+
